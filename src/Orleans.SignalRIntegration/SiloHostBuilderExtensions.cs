2ffdf802cbd0144f13850fb590eb413fa5c60896
jbockle <otakujb@gmail.com>
2020-09-01 08:23:35 -0400

    add extension method for ISiloBuilder to support aspnetcore with orleans co-hosted

--- src/Orleans.SignalRIntegration/SiloHostBuilderExtensions.cs
+++ src/Orleans.SignalRIntegration/SiloHostBuilderExtensions.cs
@@ -29,5 +29,29 @@ namespace Orleans.SignalRIntegration
 
             return builder;
         }
+
+        public static ISiloBuilder UseSignalR(this ISiloBuilder builder,
+            Action<OrleansSignalRSiloOptions> configure = null)
+        {
+            var siloOptions = new OrleansSignalRSiloOptions();
+            configure?.Invoke(siloOptions);
+
+            builder.AddSimpleMessageStreamProvider(OrleansSignalRConstants.StreamProviderName, options =>
+            {
+                options.FireAndForgetDelivery = siloOptions.FireAndForgetDelivery;
+            });
+
+            if (siloOptions.AddApplicationParts)
+                builder.ConfigureApplicationParts(manager =>
+                    { manager.AddApplicationPart(typeof(ClientGrain).Assembly).WithReferences(); });
+
+            if (siloOptions.RegisterDefaultSignalRGrainStorage)
+                builder.AddMemoryGrainStorage(OrleansSignalRConstants.StorageProviderName);
+
+            if (siloOptions.RegisterDefaultPubSubStorage)
+                builder.AddMemoryGrainStorage("PubSubStore");
+
+            return builder;
+        }
     }
 }
